390f4052b8e395c77293862973b8e53539f9839c
BastardAcademic <32774879+bastardacademic@users.noreply.github.com>
2024-05-19 12:47:17 +0100

    Initial commit with project structure and files

---
+++ Ownd-backend/models/Habit.js
@@ -0,0 +1,56 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Habit = sequelize.define('Habit', {
+  habitId: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  frequency: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  startDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  endDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  submissiveId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'userId',
+    },
+  },
+  dominantId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'userId',
+    },
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+module.exports = Habit;
