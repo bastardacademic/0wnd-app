dcd01543cd0d8624edc8a57dbd917d34f4ffa940
BastardAcademic <32774879+bastardacademic@users.noreply.github.com>
2024-12-01 10:34:25 +0000

    Expand Localization with French, German, and Spanish support

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,16 +1,24 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import translations from './locales';
+
+import enTranslation from './locales/en/translation.json';
+import frTranslation from './locales/fr/translation.json';
+import deTranslation from './locales/de/translation.json';
+import esTranslation from './locales/es/translation.json';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: translations,
-    interpolation: {
-      escapeValue: false
-    }
+    resources: {
+      en: { translation: enTranslation },
+      fr: { translation: frTranslation },
+      de: { translation: deTranslation },
+      es: { translation: esTranslation }
+    },
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false },
   });
 
 export default i18n;
