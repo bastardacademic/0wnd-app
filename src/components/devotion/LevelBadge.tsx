4a7ed18567a59e63f7b4af47e4d4259ab9ed8a57
BastardAcademic <32774879+bastardacademic@users.noreply.github.com>
2025-04-24 17:33:27 +0100

    🔖 Add reusable LevelBadge component

---
+++ src/components/devotion/LevelBadge.tsx
@@ -0,0 +1,9 @@
+﻿import React from "react";
+
+export const LevelBadge = ({ label }) => {
+  return (
+    <span className="inline-block px-2 py-1 text-xs font-semibold rounded bg-purple-800 text-white">
+      {label}
+    </span>
+  );
+};
