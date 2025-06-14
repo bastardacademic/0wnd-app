06e3cadb3645e7c9ff0eec8865e9661ac1078e2e
BastardAcademic <32774879+bastardacademic@users.noreply.github.com>
2025-06-14 16:07:37 +0100

    Remove unused frontend directory

--- frontend/src/services/JournalService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const createJournalEntry = (journalData) => {
-    return axios.post('/api/journals', journalData);
-};
-
-export const fetchJournalEntries = () => {
-    return axios.get('/api/journals');
-};
