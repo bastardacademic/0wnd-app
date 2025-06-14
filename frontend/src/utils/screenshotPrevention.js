bd52ed97c2cecdc4b90ad49d2cb99e3f0f9cd573
BastardAcademic <32774879+bastardacademic@users.noreply.github.com>
2025-06-14 15:56:18 +0100

    chore: remove obsolete frontend folder

--- frontend/src/utils/screenshotPrevention.js
+++
@@ -1,8 +0,0 @@
-export function preventScreenshot() {
-    document.addEventListener('visibilitychange', () => {
-        if (document.visibilityState === 'hidden') {
-            alert('Screenshots are disabled for this content.');
-        }
-    });
-}
-// Screenshot Prevention code...
